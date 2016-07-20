3e404987af64408b9b7c2d1b21380c7d4450ae2b
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2016-07-20 10:20:17 -0500

    Canvas Lines
    Added a canvas with dotted lines connecting services.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  drawLines();
+  window.onresize = function (event) {
+    drawLines();
+  };
+
   //play background video for android devices
   var video = document.getElementById('video');
   video.play();
@@ -7,14 +12,14 @@ $(document).ready(function() {
   var leftHeight = $('.content-left').height() * -1;
   $('.content-right').css('margin-top', leftHeight);
 
-  $(document).scroll(function() {
+  $(document).scroll(function () {
     //remove transparent class from header
     if ($(document).scrollTop() > 5) {
       if (!$('#nav-container').hasClass('nav-body')) {
         $('#nav-container').addClass('nav-body');
       }
     }
-    
+
     //add transparent class to header
     if ($(document).scrollTop() < 5) {
       if ($('#nav-container').hasClass('nav-body')) {
@@ -24,21 +29,21 @@ $(document).ready(function() {
   });
 
   //show mobile dropdown menu
-  $('#nav .icon').click(function() {
+  $('#nav .icon').click(function () {
     $('#mobile-dropdown').fadeToggle('fast');
   });
-  
+
   //Animate Portfolio Captions
-	$('.boxgrid.captionfull').hover(function(){
-	  $(".cover", this).stop().animate({top:'0px'},{queue:false,duration:160});
-	}, function() {
+  $('.boxgrid.captionfull').hover(function () {
+    $(".cover", this).stop().animate({ top: '0px' }, { queue: false, duration: 160 });
+  }, function () {
     var tileHeight = $(this).height();
-	  $(".cover", this).stop().animate({top:tileHeight},{queue:false,duration:160});
-	});
+    $(".cover", this).stop().animate({ top: tileHeight }, { queue: false, duration: 160 });
+  });
 
   //smooth scrolling anchor tags - https://css-tricks.com/snippets/jquery/smooth-scrolling/
-  $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
+  $(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -51,4 +56,51 @@ $(document).ready(function() {
       }
     });
   });
+
+  function drawLines() {
+    var servicesBackground = document.getElementById("servicesBackground");
+    var ctx = servicesBackground.getContext("2d");
+    var parentWidth = $('#services').width();
+    var parentHeight = $('#services').height();
+    ctx.canvas.height = parentHeight;
+    ctx.canvas.width = parentWidth;
+    ctx.clearRect(0, 0, parentWidth, parentHeight);
+    ctx.setLineDash([15, 5]);
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = '#A1CFE6';
+    ctx.beginPath();
+
+    var businessPointX = $('#businessService').position().left + ($("#businessService").outerWidth() / 2);
+    var businessPointY = ($("#businessService").offset().top - $("#services").offset().top) + ($("#businessService").outerHeight() / 2);
+    var devPointX = $('#devService').position().left + ($("#devService").outerWidth(true) / 2);
+    var devPointY = ($("#devService").offset().top - $("#services").offset().top) + ($("#devService").outerHeight() / 2);
+    var netPointX = $('#netService').position().left + ($("#netService").outerWidth() / 2);
+    var netPointY = ($("#netService").offset().top - $("#services").offset().top) + ($("#netService").outerHeight() / 2);
+    var servicePointX = $('#serviceService').position().left + ($("#serviceService").outerWidth() / 2);
+    var servicePointY = ($("#serviceService").offset().top - $("#services").offset().top) + ($("#serviceService").outerHeight() / 2);
+
+    if ($(window).width() <= 1250) {
+      ctx.moveTo(businessPointX, businessPointY);
+      ctx.lineTo(servicePointX, servicePointY);
+      ctx.stroke();
+    } else {
+      ctx.moveTo(businessPointX, businessPointY);
+      ctx.lineTo(businessPointX, servicePointY);
+      ctx.stroke();
+      ctx.moveTo(businessPointX, servicePointY);
+      ctx.lineTo(servicePointX, servicePointY);
+      ctx.stroke();
+
+      ctx.moveTo(netPointX, netPointY);
+      ctx.lineTo(netPointX, servicePointY);
+      ctx.stroke();
+      ctx.moveTo(netPointX, servicePointY);
+      ctx.lineTo(servicePointX, servicePointY);
+      ctx.stroke();
+
+      ctx.moveTo(devPointX, devPointY);
+      ctx.lineTo(servicePointX, servicePointY);
+      ctx.stroke();
+    }
+  }
 });
